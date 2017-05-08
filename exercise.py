0f448c933a4df0f56929431c81b2f89441b02566
denissov <nikolai.denissov@gmail.com>
2017-05-08 13:26:48 +0300

    redoing 3

--- exercise.py
+++ exercise.py
@@ -1,60 +1,7 @@
-# function for verifying installation
+# exercising PyCharm
+
 
 def main():
-    print("I Print, therefore I Am.")
-
-if __name__ == "__main__":
-    main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
+    print("Hello World!")
+if __name__ == '__main__':
     main()
